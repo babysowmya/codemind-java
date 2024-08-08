be6d5837c4d005738397f73a6db0dc7ec3612c3e
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-08-08 19:41:05 +0530

    import java.util.Scanner; public class Example{     public static void main(String[] args)     {         int a;         Scanner read=new Scanner(System.in);         a=read.nextInt();         if(a%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

--- Even_or_Odd.java
+++ Even_or_Odd.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Example{
     public static void main(String[] args)
     {
-        Scanner read=new Scanner(System.in);
         int a;
+        Scanner read=new Scanner(System.in);
         a=read.nextInt();
         if(a%2==0)
         {
