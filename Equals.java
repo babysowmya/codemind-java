0ced430ccbb49767acc85ad23807efbeea8ab50a
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-08-21 11:31:20 +0530

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Equals  {
+    public static void main(String[] args) {
+        int[] A = {10, 20, 30};
+        int[] B = {10, 20, 30};
+         boolean result = Arrays.equals(A,B);
+         System.out.println(result);
+    }
+}
