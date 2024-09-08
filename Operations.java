47a39deccb34d29f1b13cbe23603bb71c3fccf8b
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-09-08 08:35:40 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,56 @@
+/*class ArthematicOperations{
+    void add(int a,int b){
+        System.out.println(a+b);
+    }
+    void substraction(int a,int b)
+    {
+        System.out.println(a-b);
+    }
+    void multiply(int a,int b)
+    {
+        System.out.println(a*b);
+    }
+    void divide(int a,int b)
+    {
+         System.out.println(a/b);
+    }
+}
+public class Operations {
+   public static void main(String[] args) {
+    
+    ArthematicOperations ae = new ArthematicOperations();
+    ae.add(10,20);
+    ae.substraction(10,2);
+    ae.multiply(10,3);
+    ae.divide(10,2);
+   }
+}*/
+
+
+
+
+class ArthematicOperations{
+     static void add(int a,int b){
+        System.out.println(a+b);
+    }
+     static void substraction(int a,int b)
+    {
+        System.out.println(a-b);
+    }
+    static void multiply(int a,int b)
+    {
+        System.out.println(a*b);
+    }
+     static void divide(int a,int b)
+    {
+         System.out.println(a/b);
+    }
+}
+public class Operations {
+   public static void main(String[] args) {
+    ArthematicOperations.add(10,20);
+    ArthematicOperations.substraction(10,2);
+    ArthematicOperations.multiply(10,3);
+    ArthematicOperations.divide(10,2);
+   }
+}
