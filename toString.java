f6db1b68ce13082a039ed3a75082417f3cf5f2db
babysowmya <147791943+babysowmya@users.noreply.github.com>
2024-09-13 11:32:02 +0530

    Add files via upload

---
+++ toString.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class toString {
+    public static void main(String[] args)
+    {
+        int[] arr = {10,20,30};
+        String[] strArray = {"abc","xyz","pqr"};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(strArray));
+    }
+}
